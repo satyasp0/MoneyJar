9bd0cc754aff3cc81fb78529332dff5b4fa8ade6
Satya <agussetiawanpopalia@gmail.com>
2024-07-03 15:04:46 +0700

    initial commit

---
+++ src/main/java/org/persona/moneyjar/MoneyjarApplication.java
@@ -0,0 +1,13 @@
+package org.persona.moneyjar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyjarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyjarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/persona/moneyjar/MoneyjarApplicationTests.java
@@ -0,0 +1,13 @@
+package org.persona.moneyjar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyjarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
